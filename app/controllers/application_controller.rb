2c79d68e4609730bd470e2dbc7a9835eba7c9190
Angel Resendiz <angel.resendiz@gmail.com>
2022-07-24 22:43:19 -0500

    Rescuing and redirect unauthorized actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   after_action :verify_authorized, unless: :devise_controller?
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform the requested action."
+    redirect_back(fallback_location: root_path)
+  end
 end
